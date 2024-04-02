b49adcd2b35ab38062f83f7068f64eb26091efc9
Vaibhav  Gaikwad <124031895+vaibhavgaikwad8476@users.noreply.github.com>
2024-04-02 11:04:45 +0530

    Update AboutMe.js

--- src/AboutUs/AboutMe.js
+++ src/AboutUs/AboutMe.js
@@ -142,11 +142,12 @@ function AboutMe() {
 
         <div class="skillsedication">
           <div class="educations">
+                <p class="courseyear">July 2021 - June 2024</p>
             <h3 class="Subeduhead">Bachelor's of  Engineering, Computer Engineering</h3>
             <div class="educationDescription">
               <p>College: Hon. Shri Babanroa Pachlute Vichardhara trusts group of institutions College of Engineering, Ahmednagar</p>
               <p id="grade">SGPA: 7.21</p>
-              <i class="courseyear">July 2021 - June 2024</i>
+             
 
             </div>
             <br></br>
@@ -154,11 +155,12 @@ function AboutMe() {
 
 
           <div class="educations">
+              <p class="courseyear">July 2019 - May 2021</p>
             <h3 class="Subeduhead">Diploma, Mechanical Engineering</h3>
             <div class="educationDescription">
               <p>College: Government polytechnic Kolhapur, KOlhapur</p>
               <p id="grade">Percentage: 89.71</p>
-              <i class="courseyear">July 2019 - May 2021</i>
+              
 
             </div>
             <br></br>
@@ -166,23 +168,21 @@ function AboutMe() {
 
 
           <div class="educations">
+               <p class="courseyear">June 2018 - Feb 2019</p>
             <h3 class="Subeduhead">Higher Secondary, Science</h3>
             <div class="educationDescription">
               <p>College: Shri Aranyeshwar Vidyalaya and Junior College Arangoan</p>
               <p id="grade">Percentage: </p>
-              <i class="courseyear">June 2018 - Feb 2019</i>
-
             </div>
             <br></br>
           </div>
 
           <div class="educations">
+              <p class="courseyear">June 2017</p>
             <h3 class="Subeduhead">Secondary School, Semi-english</h3>
             <div class="educationDescription">
               <p>College: Shri vitthal vidyalaya Mahijalgon</p>
-              <p id="grade">Percentage: </p>
-              <i class="courseyear">June 2017</i>
-
+              <p id="grade">Percentage: </p>  
             </div>
             <br></br>
           </div>
